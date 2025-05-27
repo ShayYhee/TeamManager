7b6a22157e4dd1149f977b1a40abb71c70b37285
ShayYhee <faithosebi02@gmail.com>
2025-05-27 17:17:16 +0300

    Switch to Docker deployment with wkhtmltopdf

--- raadaa/settings.py
+++ raadaa/settings.py
@@ -22,7 +22,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'insecure-key-for-dev-only')
 DEBUG = os.getenv('DJANGO_DEBUG', 'False') == 'True'
 # ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', 'localhost').split(',')
 ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "*").split(",")
-WKHTMLTOPDF_PATH = os.getenv('WKHTMLTOPDF_PATH', '/usr/local/bin/wkhtmltopdf')
+WKHTMLTOPDF_PATH = os.environ.get("WKHTMLTOPDF_PATH", "/usr/bin/wkhtmltopdf")
 
 PDFKIT_CONFIG = {
     'wkhtmltopdf': WKHTMLTOPDF_PATH
