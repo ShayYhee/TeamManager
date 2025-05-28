bb6ab7c230249c33c5afbab05b9fa39794de384a
ShayYhee <faithosebi02@gmail.com>
2025-05-28 14:44:48 +0300

    Edited url fordeployment

--- raadaa/settings.py
+++ raadaa/settings.py
@@ -22,7 +22,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'insecure-key-for-dev-only')
 DEBUG = os.getenv('DJANGO_DEBUG', 'False') == 'True'
 # ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', 'localhost').split(',')
 # ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "*").split(",")
-ALLOWED_HOSTS = ['raadaa.onrender.com']
+ALLOWED_HOSTS = ['https://raadaa.onrender.com']
 # WKHTMLTOPDF_PATH = os.environ.get("WKHTMLTOPDF_PATH", "/usr/bin/wkhtmltopdf")
 
 # PDFKIT_CONFIG = {
