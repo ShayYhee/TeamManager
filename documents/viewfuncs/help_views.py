6138ae2c4b4d69971609e75979c7d9a5c31efafe
ShayYhee <faithosebi02@gmail.com>
2025-10-23 21:31:08 +0300

    Edited support contact

--- documents/viewfuncs/help_views.py
+++ documents/viewfuncs/help_views.py
@@ -1,5 +1,6 @@
 # For Help and Support
 from documents.forms import SupportForm
+from documents.models import CustomUser
 from django.http import JsonResponse
 from django.shortcuts import render
 from django.core.mail import EmailMessage
@@ -11,6 +12,16 @@ def contact_support(request):
         files = request.FILES.getlist('attachments')
         print("FILES data: %s", request.FILES)
         print(("FILES data: %s", [(f.name, f.size, f.content_type) for f in files] if files else "No files received"))
+        user = request.user
+        superuser = CustomUser.objects.filter(is_active=True, is_superuser=True).first()
+        if user.email_provider and user.email_address and user.email_password:
+            sender_provider = user.email_provider
+            sender_email = user.email_address
+            sender_password = user.email_password
+        else:
+            sender_provider = superuser.email_provider
+            sender_email = superuser.email_address
+            sender_password = superuser.email_password
         form = SupportForm(request.POST, request.FILES)
         if form.is_valid():
             try:
@@ -19,8 +30,9 @@ def contact_support(request):
                     subject=form.cleaned_data['subject'],
                     body=form.cleaned_data['message'],
                     from_email=request.user.email_address if request.user.is_authenticated else 'no-reply@teammanager.ng',
-                    to=['faith.osebi@transnetcloud.com'],
-                    connection=get_email_smtp_connection(request.user.email_provider, request.user.email_address, request.user.email_password)
+                    to=['contact@teammanager.ng'],
+                    cc=[request.user.email] if not request.user.email_address else [],
+                    connection=get_email_smtp_connection(sender_provider, sender_email, sender_password)
                 )
                 
                 # Handle attachments
